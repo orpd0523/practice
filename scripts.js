62a809a257737cec5418bb2d1333f679e3066f20
orpd0523 <orpd0523@gmail.com>
2023-04-13 15:52:55 -0500

    created logic for menu toggling

--- scripts.js
+++ scripts.js
@@ -1,1 +1,27 @@
-console.log("I am connected!")
+// console.log("I am connected!")
+
+const menu = document.querySelector('.nav__links')
+const items = document.querySelectorAll('.item')
+const burger = document.querySelector('.nav__burger')
+const close = document.querySelector('.closeIcon')
+const open = document.querySelector('.menuIcon')
+
+const toggleMenu = () => {
+    if (menu.classList.contains("showMenu")) {
+        menu.classList.remove('showMenu')
+        close.style.display = "none"
+        open.style.display = "block"
+    } else {
+        menu.classList.add("showMenu")
+        close.style.display = "block"
+        open.style.display = "none"
+    }
+}
+
+burger.addEventListener("click", toggleMenu)
+
+items.forEach(
+    function (items) {
+        items.addEventListener("click", toggleMenu)
+    }
+)
