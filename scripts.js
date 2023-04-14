051049f6d690cae6c955100f9948258282896a71
orpd0523 <orpd0523@gmail.com>
2023-04-13 19:31:20 -0500

    created toggle menu

--- scripts.js
+++ scripts.js
@@ -1,21 +1,25 @@
 // console.log("I am connected!")
 
 const menu = document.querySelector('.nav__links')
-const items = document.querySelectorAll('.item')
+const items = document.querySelectorAll('.nav__item')
 const burger = document.querySelector('.nav__burger')
 const close = document.querySelector('.closeIcon')
 const open = document.querySelector('.menuIcon')
+const button = document.getElementById('#button')
 
 const toggleMenu = () => {
-    if (menu.classList.contains("showMenu")) {
-        menu.classList.remove('showMenu')
+    if (menu.classList.contains("nav__links")) {
+        menu.classList.remove('nav__links')
+        menu.style.visibility = "hidden"
         close.style.display = "none"
         open.style.display = "block"
     } else {
-        menu.classList.add("showMenu")
+        menu.classList.add("nav__links")
         close.style.display = "block"
         open.style.display = "none"
+        menu.style.visibility = "visible"
     }
+    
 }
 
 burger.addEventListener("click", toggleMenu)
@@ -25,3 +29,15 @@ items.forEach(
         items.addEventListener("click", toggleMenu)
     }
 )
+
+const hide = () => {
+    let element = document.getElementById("hide")
+    element.classList.toggle('hidden');
+    // if (element.style.display === "none") {
+        // element.style.display = "block"
+    // } else {
+        // element.style.display = "visible"
+    // }
+}
+
+// hide(button)
